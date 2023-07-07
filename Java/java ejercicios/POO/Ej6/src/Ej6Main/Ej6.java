8b1c308d84b0585f7f9449411059527030eb4595
kbcruz6 <agustintcruz@gmail.com>
2023-07-06 21:54:59 -0300

    llenarcafetera

--- Java/java ejercicios/POO/Ej6/src/Ej6Main/Ej6.java
+++ Java/java ejercicios/POO/Ej6/src/Ej6Main/Ej6.java
@@ -1,6 +1,12 @@
+import Ej6Entities.Nespresso;
+import Ej6Services.ServiceEj6;
+
 public class Ej6 {
 
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
+    ServiceEj6 se6 = new ServiceEj6();
+
+    Nespresso n1 = se6.crearCafetera();
+    se6.llenarCafetera(n1);
   }
 }
--- Java/java ejercicios/POO/Ej6/src/Ej6Services/ServiceEj6.java
+++ Java/java ejercicios/POO/Ej6/src/Ej6Services/ServiceEj6.java
@@ -1,21 +1,41 @@
 package Ej6Services;
 
+import Ej6Entities.Nespresso;
 import java.util.Scanner;
 
-import Ej6Entities.Nespresso;
-
 public class ServiceEj6 {
+
   Scanner read = new Scanner(System.in);
 
-  public Nespresso crearCafetera(Nespresso n1){
+  public Nespresso crearCafetera() {
     System.out.println("----------------------------------------");
-    System.out.println("Ingrese capacidad maxima de la cafetera:");
+    System.out.println("Ingrese capacidad maxima de la cafetera (ml):");
     int capacidadMaxima = read.nextInt();
     System.out.println("----------------------------------------");
-    System.out.println("Ingrese cantidad actual de cafe:");
-    int cantidadActual=read.nextInt();
+    System.out.println("Ingrese cantidad actual de cafe (ml):");
+    int cantidadActual = read.nextInt();
     System.out.println("----------------------------------------");
 
-    return new Nespresso(0, 0)
+    return new Nespresso(capacidadMaxima, cantidadActual);
+  }
+
+  public void llenarCafetera(Nespresso n1) {
+    System.out.println("Llenando cafetera...");
+    try {
+      Thread.sleep(1500);
+      System.out.println(
+        "Se agregaron " +
+        (n1.getCapacidadMaxima() - n1.getCantidadActual()) +
+        " ml"
+      );
+      n1.setCantidadActual(n1.getCapacidadMaxima());
+
+      System.out.println(
+        "¡Cafetera llena nuevamente con: " + n1.getCantidadActual() + " ml !"
+      );
+      System.out.println("-----------------------------------------");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
