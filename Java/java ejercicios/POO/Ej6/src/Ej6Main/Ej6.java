5e9f9d0b881464fd4cff80150c5f88c1d635725b
kbcruz6 <agustintcruz@gmail.com>
2023-07-10 20:43:25 -0300

    asd

--- Java/java ejercicios/POO/Ej6/src/Ej6Main/Ej6.java
+++ Java/java ejercicios/POO/Ej6/src/Ej6Main/Ej6.java
@@ -8,5 +8,6 @@ public class Ej6 {
 
     Nespresso n1 = se6.crearCafetera();
     se6.llenarCafetera(n1);
+    se6.servirTaza(n1);
   }
 }
--- Java/java ejercicios/POO/Ej6/src/Ej6Services/ServiceEj6.java
+++ Java/java ejercicios/POO/Ej6/src/Ej6Services/ServiceEj6.java
@@ -38,4 +38,29 @@ public class ServiceEj6 {
       e.printStackTrace();
     }
   }
+
+  public void servirTaza(Nespresso n1) {
+    System.out.println("Ingrese capacidad de la taza: ");
+    int capacidadTaza = read.nextInt();
+    System.out.println("-------------------------------------------");
+    System.out.println("Sirviendo taza...");
+
+    try {
+      Thread.sleep(1500);
+      if (n1.getCantidadActual() >= capacidadTaza) {
+        n1.setCantidadActual(n1.getCantidadActual() - capacidadTaza);
+        System.out.println(
+          "En la cafetera quedaron: " + n1.getCantidadActual() + " ml"
+        );
+        System.out.println("-------------------------------------------");
+      } else {
+        System.out.println(
+          "Se han podido servir " + n1.getCantidadActual() + " ml unicamente."
+        );
+        System.out.println("-------------------------------------------");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
